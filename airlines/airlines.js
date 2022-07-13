e9faf575ab8ccbcc6325f10d94c78357ac6c9cbe
Natalia Gherardi <natirgh@gmail.com>
2022-07-13 20:40:47 +0200

    fixed eslint corrections in airlines project

--- airlines/airlines.js
+++ airlines/airlines.js
@@ -1,23 +1,25 @@
-let flights = [
-  { id: 00, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+const flights = [
+  { id: 0, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
 
-  { id: 01, to: "New York", from: "Barcelona", cost: 700, scale: false },
+  { id: 1, to: "New York", from: "Barcelona", cost: 700, scale: false },
 
-  { id: 02, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
+  { id: 2, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
 
-  { id: 03, to: "Paris", from: "Barcelona", cost: 210, scale: false },
+  { id: 3, to: "Paris", from: "Barcelona", cost: 210, scale: false },
 
-  { id: 04, to: "Roma", from: "Barcelona", cost: 150, scale: false },
+  { id: 4, to: "Roma", from: "Barcelona", cost: 150, scale: false },
 
-  { id: 05, to: "London", from: "Madrid", cost: 200, scale: false },
+  { id: 5, to: "London", from: "Madrid", cost: 200, scale: false },
 
-  { id: 06, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
+  { id: 6, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
 
-  { id: 07, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
+  { id: 7, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
 
-  { id: 08, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
+  { id: 8, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
 
-  { id: 09, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
+  { id: 9, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
 
   { id: 10, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
 ];
@@ -29,7 +31,7 @@ const askNameAndGreet = () => {
   console.log(`Bienvenido, ${name}!`);
 };
 
-const showFlights = (flight) => {
+const showFlights = () => {
   vuelosConEscala.forEach((flight) =>
     console.log(
       `El vuelo con origen: ${flight.from}, y destino: ${flight.to} tiene un coste de ${flight.cost} y realiza alguna escala.`
@@ -45,9 +47,7 @@ const showFlights = (flight) => {
   console.log(`Los vuelos que efectúan escalas son ${vuelosConEscala.length}`);
 };
 const promedio = () => {
-  const suma = flights.reduce((a, vuelo) => {
-    return a + vuelo.cost;
-  }, 0);
+  const suma = flights.reduce((a, vuelo) => a + vuelo.cost, 0);
   console.log(
     `El costo promedio de los vuelos es de ${(suma / flights.length).toFixed(
       2
@@ -56,8 +56,8 @@ const promedio = () => {
 };
 const mostrarUltVuelos = () => {
   const ultimosVuelos = flights.slice(-5);
-  let destinosUltimosVuelos = ultimosVuelos.map(
-    (ultimosVuelos) => ultimosVuelos.to
+  const destinosUltimosVuelos = ultimosVuelos.map(
+    (vuelosUltimos) => vuelosUltimos.to
   );
   console.log(
     `Los destinos de los últimos vuelos son: ${destinosUltimosVuelos}`
